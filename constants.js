f17eeeb181b804e20bf60aaeaff3dd78f379343c
Darius Foodeei <darius.foodei@gmail.com>
2022-10-26 20:01:27 +0200

    mod const

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const contractAddress = "0xE0e41CF578594fc1dd9D6A2451251b8bdbd16eF5";
 export const abi = [
     {
         inputs: [
